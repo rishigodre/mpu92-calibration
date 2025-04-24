7309657bf36ce7d9e197d707aaa18ad42bed1ba0
rishigodre <rishigodre14@gmail.com>
2025-04-24 23:32:58 +0530

    fall detection implimented

--- fall_detection/fallDetection.py
+++ fall_detection/fallDetection.py
@@ -1,28 +1,32 @@
 import time
-from mpu9250_i2c import *
-time.sleep(0.5)
-#
-#############################
-# Strings for Units/Labs
-#############################
-#
-imu_devs   = ["ACCELEROMETER"]
-imu_labels = ["x-dir","y-dir","z-dir"]
-imu_units  = ["g","g","g"]
-
-while True:
-    ##################################
-    # Reading and Printing IMU values
-    ##################################
-    #
+t0 = time.time()
+start_bool = False # boolean for connection
+while (time.time()-t0)<5: # wait for 5-sec to connect to IMU
     try:
-        ax,ay,az,extra = mpu6050_conv() # read and convert mpu6050 data
+        from mpu9250_i2c import *
+        start_bool = True # True for forthcoming loop
+        break
     except:
         continue
+#############################
+# Main Loop to Test IMU
+#############################
+#
+oldG = 1
+while True:
+    if start_bool==False: # make sure the IMU was started
+        print("IMU not Started, Check Wiring") # check wiring if error
+        break
+    try:
+        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+    except:
+        continue
+    netG = ax * ax + ay * ay + az * az
+    netG = netG ** 0.5
+    jerkMag = (netG - oldG) / (0.001)
 
-    netG = (ax*ax + ay*ay + az*az)**0.5
+    if jerkMag > 1500:
+        print("fall!!")
 
-    print(netG + " - net G")
-
-    time.sleep(0.01)
+    time.sleep(0.001) # wait between prints
 
